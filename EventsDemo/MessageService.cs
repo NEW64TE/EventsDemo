e4ac6ca8cd1bdb9cb7b22045e6fed4f6cbfdf978
Eduard Gross <56992766+NEW64TE@users.noreply.github.com>
2022-07-21 18:18:23 +0200

    Create MessageService.cs and instance

---
+++ EventsDemo/MessageService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace EventsDemo
+{
+    partial class Program
+    {
+        public class MessageService
+        {
+            public void OnVideoEncoded(object source, EventArgs e)
+            {
+                Thread.Sleep(3000);
+                Console.WriteLine("MailService: sending an email...");
+            }
+        }
+    }
+}
