703e9ce6eea6e1361f3fafad0dc9a735735ceb47
Eduard Gross <56992766+NEW64TE@users.noreply.github.com>
2022-07-21 18:02:36 +0200

    Create instances of Video and VideoEncoder in Programm.cs

--- EventsDemo/Program.cs
+++ EventsDemo/Program.cs
@@ -6,7 +6,10 @@ namespace EventsDemo
     {
         static void Main(string[] args)
         {
-
+            var video = new Video() { Title = "Video 1" };
+            var videoEncoder = new VideoEncoder();
+            
+            videoEncoder.Encode(video);
         }
     }
 }
