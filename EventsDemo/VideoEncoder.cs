53f27b17f81602d576d7fe9b72607477e99e7667
Eduard Gross <56992766+NEW64TE@users.noreply.github.com>
2022-07-21 17:52:43 +0200

    Create VideoEncoder.cs

---
+++ EventsDemo/VideoEncoder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+
+namespace EventsDemo
+{
+    public class VideoEncoder 
+    {
+        public void Encode(Video video)
+        {
+            Console.WriteLine("Encoding Video...");
+            Thread.Sleep(3000);
+        }
+    }
+}
