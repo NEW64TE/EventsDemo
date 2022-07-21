6fd5a9ddcfe8c29d016e216bfb36b56c123d4bf3
Eduard Gross <56992766+NEW64TE@users.noreply.github.com>
2022-07-21 17:57:56 +0200

    Add OnvideoEncoded handler and method

--- EventsDemo/VideoEncoder.cs
+++ EventsDemo/VideoEncoder.cs
@@ -5,10 +5,24 @@ namespace EventsDemo
 {
     public class VideoEncoder 
     {
+        public delegate void VideoEncodedEventHandler(object source, EventArgs args);
+
+        public event VideoEncodedEventHandler VideEncoded;
+
         public void Encode(Video video)
         {
             Console.WriteLine("Encoding Video...");
             Thread.Sleep(3000);
+
+            OnVideoEncoded();
+        }
+
+        protected virtual void OnVideoEncoded()
+        {
+            if (VideEncoded != null)
+            {
+                VideEncoded(this, EventArgs.Empty);
+            }
         }
     }
 }
