c382fafe4f8f2e71e6913528ece88af4a66ba761
Eduard Gross <56992766+NEW64TE@users.noreply.github.com>
2022-07-21 16:08:08 +0200

    Add project files.

---
+++ EventsDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EventsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
